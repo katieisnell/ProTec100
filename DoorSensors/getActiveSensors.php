2f13016af90b00bcffe29d6da66a937f56240d1c
katieisnell <katie.snell@student.manchester.ac.uk>
2017-10-29 12:41:01 +0000

    Finds all sensors on the 2nd floor of the Museum

--- DoorSensors/getActiveSensors.php
+++ DoorSensors/getActiveSensors.php
@@ -1,23 +1,29 @@
 <?php
 
 require_once('config.inc.php');
-$conn = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
-
+$conn = new mysqli($database_host, $database_user, $database_pass, $database_name);
+if (mysqli_connect_errno())
+{
+    echo "An error occured. Please try again later.";
+    exit();
+}
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
 
 // Code to find active sensors
 $date_now = new DateTime();
 
 // Remove 30 seconds from the current time
 date_sub($date_now, date_interval_create_from_date_string('30 seconds'));
+$date_now->modify('-1 hours');
 
 // Convert to date string so php can compare it
 $date_now_string = $date_now->format('Y-m-d H:i:s');
 
-$sqlGetEverythingRicochet = "SELECT * FROM ricochet WHERE Time>'".$date_now_string."' ";
+$sqlGetEverythingRicochet = "SELECT * FROM ricochet WHERE Time>'".$date_now_string."' AND ExpanderId=3";
 
 $resultGetEverythingRicochet = $conn->query($sqlGetEverythingRicochet);
 
